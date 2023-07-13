fa6ee00d01882845ec56ffb155f43fb4e67a64c6
Brunobmendes <Bruno.mendes2804@gmail.com>
2023-07-13 19:15:43 -0300

    create project base

---
+++ src/main/java/com/psai/podcastrestAPI/PodcastRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.psai.podcastrestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psai/podcastrestAPI/PodcastRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psai.podcastrestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
